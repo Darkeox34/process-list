ea880230caf946b9f99308f4b61fd2d19cbaa012
Darkeox34 <antonio.consoli2003@gmail.com>
2022-10-09 17:02:16 +0200

    End

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+
+void getProcessList(){
+HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+if(hSnap != INVALID_HANDLE_VALUE){
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(PROCESSENTRY32);
+
+    if(Process32First(hSnap, &pe)){
+        do{
+            std::wcout << pe.szExeFile << " " << pe.th32ProcessID << std::endl;
+        }while(Process32Next(hSnap, &pe));
+    }
+}
+
+}
 
 int main(){
 
-    
+    getProcessList();
 }
